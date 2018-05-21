74829e49e1e6f6bf0acc867ec0d03187150422ba
yuxichen11@gmail.com <yuxichen11@gmail.com>
2018-05-21 20:24:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,11 +13,19 @@ function logWhisper(string) {
   console.log(Whisper);
 }
 function sayHiToGrandma(string) {
+<<<<<<< HEAD
   var GrandmaWhisper = string
   var GrandmaShout = string
   if (GrandmaWhisper.toLowerCase() === GrandmaWhisper) {
     return "I can't hear you!";
   }else if (GrandmaShout.toUpperCase() === GrandmaShout) {
+=======
+  var GrandmaWhisper = "lowercase";
+  var GrandmaShout = "UPPERCASE";
+  if (string.toLowerCase() === GrandmaWhisper) {
+    return "I can't hear you!";
+  }else if (string.toUpperCase() === GrandmaShout) {
+>>>>>>> db00009edb3e107f1fc8a196a3efb20dd0151796
     return "YES INDEED!";
   }else {
     return "I love you, too.";
